a94742be6e8721041f7c3a26c741ee9a376d6762
gbteixeira <gbteixeira.profissional@gmail.com>
2018-10-16 01:38:50 -0300

    Home Sweet Home

---
+++ refs/home/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Git</title>
+  </head>
+  <body>
+    <h1>Git course to Gabriel project</h1>
+    <h6>Laravel + VueJs </h6>
+  </body>
+</html>
